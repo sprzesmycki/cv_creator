f8e8989ac7479e7aa26d7551a5c6ed1351ca435f
Sebastian Przesmycki <sebastian.przesmycki@gmail.com>
2022-08-17 18:36:26 +0200

    added new table for user experience

--- flaskapp/database.py
+++ flaskapp/database.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../cv_creator.db'
@@ -11,12 +9,13 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    permission = Column(String, nullable=False)  # todo probably replace with another class
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    permission = db.Column(db.String, nullable=False)  # todo probably replace with another class
 
-    user_skills = relationship("UserSkills", back_populates="user")
+    user_skills = db.relationship("UserSkills", back_populates="user")
+    user_experience = db.relationship("UserExperience", back_populates="user")
 
     def __repr__(self):
         return f"User(id={self.id!r}, first_name={self.first_name!r}, last_name={self.last_name!r})"
@@ -25,10 +24,10 @@ class User(db.Model):
 class Skills(db.Model):
     __tablename__ = 'skills'
 
-    id = Column(Integer, primary_key=True)
-    skill_name = Column(String, nullable=False, unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    skill_name = db.Column(db.String, nullable=False, unique=True)
 
-    user_skills = relationship("UserSkills", back_populates="skills")
+    user_skills = db.relationship("UserSkills", back_populates="skills")
 
     def __repr__(self):
         return f"Skills(id={self.id!r}, skill_name={self.skill_name!r}"
@@ -37,14 +36,32 @@ class Skills(db.Model):
 class UserSkills(db.Model):
     __tablename__ = 'user_skills'
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    skill_id = Column(Integer, ForeignKey('skills.id'), nullable=False)
-    skill_level = Column(Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    skill_id = db.Column(db.Integer, db.ForeignKey('skills.id'), nullable=False)
+    skill_level = db.Column(db.Integer, nullable=False)
 
-    user = relationship("User", back_populates="user_skills")
-    skills = relationship("Skills", back_populates="user_skills")
+    user = db.relationship("User", back_populates="user_skills")
+    skills = db.relationship("Skills", back_populates="user_skills")
 
     def __repr__(self):
         return f"UserSkills(id={self.id!r}, user_id={self.user_id!r}, " \
                f"skill_id={self.skill_id!r}, skill_level={self.skill_level!r}) "
+
+
+class UserExperience(db.Model):
+    __tablename__ = 'user_experience'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    company_name = db.Column(db.String, nullable=False)
+    job_description = db.Column(db.String, nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)  # todo datetime or just int
+    end_date = db.Column(db.DateTime, nullable=False)  # todo datetime or just int
+
+    user = db.relationship("User", back_populates="user_experience")
+
+    def __repr__(self):
+        return f"UserExperience(id={self.id!r}, user_id={self.user_id!r}, " \
+               f"company_name={self.company_name!r}, job_description={self.job_description!r}), " \
+               f"start_date={self.start_date!r}, end_date={self.end_date!r}) "
