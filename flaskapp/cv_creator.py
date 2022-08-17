361cdc325ae880c1efd46eb2f806c975bb2367ba
Sebastian Przesmycki <sebastian.przesmycki@gmail.com>
2022-08-17 18:23:58 +0200

    added stubs for all endpoints

--- flaskapp/cv_creator.py
+++ flaskapp/cv_creator.py
@@ -1,15 +1,49 @@
-from flask import json, request
+from flask import json, request, make_response
 
 from .database import app, db, User
 
 
-@app.route('/add_user', methods=["POST"])
-def add_user():
-    data = json.loads(request.data)  # todo probably context-type validation required and handling missing fields
-    user = User(first_name=data['first_name'], last_name=data['last_name'], permission=data['permission'])
-    db.session.add(user)
-    db.session.commit()
-    return repr(user)
+@app.route('/user', methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
+def user():
+    if request.method == 'GET':
+        return make_response(
+            "I will do that sometime later",
+            418
+        )
+    if request.method == 'POST':
+        data = json.loads(request.data)  # todo probably context-type validation required and handling missing fields
+        user_details = User(first_name=data['first_name'], last_name=data['last_name'], permission=data['permission'])
+        db.session.add(user_details)
+        db.session.commit()
+        return make_response(
+            repr(user_details),
+            200
+        )
+    if request.method == 'PUT':
+        return make_response(
+            "I will do that sometime later",
+            418
+        )
+    if request.method == 'PATCH':
+        return make_response(
+            "I will do that sometime later",
+            418
+        )
+    if request.method == 'DELETE':
+        return make_response(
+            "I will do that sometime later",
+            418
+        )
+
+
+@app.route('/cv', methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
+def cv():
+    return "I will do that sometime later"
+
+
+@app.route('/stats', methods=["GET"])  # todo should I user methods=["GET"] even if its default one?
+def get_stats():
+    return "I will do that sometime later"
 
 
 if __name__ == '__main__':
