8f951f773ef6aa36208c032a88c5e66b8d774aa6
sebastian.przesmycki <sebastian.przesmycki@gmail.com>
2022-08-29 12:32:28 +0200

    adding first working tests

--- tests/test_cv_creator.py
+++ tests/test_cv_creator.py
@@ -1,4 +1,5 @@
 import pytest
+
 from cv_creator.app import create_app
 
 
@@ -33,12 +34,6 @@ def test_request_get_user(client):
 
 
 def test_request_post_user(client):
-    response = client.get("/user?userId=1")
-    assert response.status_code == 200
-    assert response.data == b'{"first_name":"seb","last_name":"as","user_experience":[],"user_skills":[]}\n'
-
-
-def test_edit_user(client):
     first_name = "seb"
     last_name = "as"
     role = "admin"
