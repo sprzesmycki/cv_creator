e44269be05f6a5851fb491421983fd85ddd30bba
Sebastian Przesmycki <sprzesmycki@pgs-soft.com>
2022-10-27 19:27:10 +0200

    Fixed issue with load on post

--- cv_creator/controllers/user_controller.py
+++ cv_creator/controllers/user_controller.py
@@ -1,5 +1,6 @@
 from cv_creator.models.models import User
 from cv_creator.storage.postgres import repository
+from cv_creator.storage.postgres.db_models import UserDb
 
 
 def get_user_by_user_id(user_id) -> User:
@@ -10,8 +11,8 @@ def add_user(post_user) -> User:
     return repository.add_user(post_user)
 
 
-def update_user(user_id, patch_user) -> User:
-    user = repository.get_user_by_user_id(user_id)
+def update_user(user_id: int, patch_user: User) -> User:
+    user: UserDb = repository.get_user_by_user_id(user_id)
     if user is not None:
         return repository.update_user(patch_user, user, user_id)
 
--- cv_creator/storage/postgres/database.py
+++ cv_creator/storage/postgres/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/postgres'
-# SQLALCHEMY_DATABASE_URI = os.environ.get("POSTGRES_DB_URI")
+from cv_creator.config import config
+
+SQLALCHEMY_DATABASE_URI = config.get("POSTGRES_DB_URI")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
